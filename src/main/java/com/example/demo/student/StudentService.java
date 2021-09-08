db0d66738c5ed2ad8461088da8be3f7d920546ab
Yannick fumukani <slashcs7@hotmail.com>
2021-09-08 00:40:13 -0500

    Student PostMapping Implementation

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -20,5 +21,15 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student){
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+
+        if(studentByEmail.isPresent()){
+            throw new IllegalStateException("Email taken");
+        }
+
+        this.studentRepository.save(student);
     }
 }
